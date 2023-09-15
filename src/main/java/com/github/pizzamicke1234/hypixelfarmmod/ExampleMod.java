75ef96c36c2093df1ac950d48c9c5712d4463fa8
GitHub Action <action@github.com>
2023-09-15 14:24:16 +0000

    Initialize template repository proper
    Set package name to com.github.pizzamicke1234.hypixelfarmmod
    Set project name to Hypixel-Farm-Mod
    Set modid to hypixelfarmmod

--- src/main/java/com/github/pizzamicke1234/hypixelfarmmod/ExampleMod.java
+++ src/main/java/com/github/pizzamicke1234/hypixelfarmmod/ExampleMod.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.github.pizzamicke1234.hypixelfarmmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "hypixelfarmmod", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/pizzamicke1234/hypixelfarmmod/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/pizzamicke1234/hypixelfarmmod/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.pizzamicke1234.hypixelfarmmod.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
